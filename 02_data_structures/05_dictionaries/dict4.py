bec821a88e5df27b3c8ff039004a6e738ab4cf50
kelvin okumu <105194367+kelvinokumu@users.noreply.github.com>
2025-05-28 10:52:49 +0300

    dict 4

--- 02_data_structures/05_dictionaries/dict4.py
+++ 02_data_structures/05_dictionaries/dict4.py
@@ -1,12 +1,12 @@
-word = "hello world"
+list_words = ["Hello","world","python"]
 word_count = {}
-for char in word:
-   if char in word_count:
-      word_count[char] += 1
+for word in list_words:
+   if word in word_count:
+      word_count[word] += 1
       # word_count[char] = word_count[char] + 1
       print(word_count)
    else:
-        word_count[char] = 1
+        word_count[word] = 1
         print(word_count)
 
 print(f"The final result is {word_count}")
